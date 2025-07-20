4422503e4217e719f599eb5fe34ad9f849b00933
Ganneboinasiva <ganneboina.siva.9705@gmail.com>
2025-07-20 22:32:43 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Visitor-Counter-App
+# Visitor Counter App
+
+Shows how many times the page has been visited.
+
+## Run Locally
+
+```bash
+docker build -t visitor-app .
+docker run -p 5000:5000 visitor-app
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+counter = 0
+
+@app.route('/')
+def visit():
+    global counter
+    counter += 1
+    return f"This page has been visited {counter} times."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
